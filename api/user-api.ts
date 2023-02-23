66362df11a59b2fcdd10b3c699a3b40b0ac73b37
Brian <briancabrer4@gmail.com>
2023-02-23 18:29:02 -0300

    get user by document and create user

--- api/user-api.ts
+++ api/user-api.ts
@@ -1,18 +1,90 @@
 import express from 'express';
+
 import { superadminMiddleware } from '../middlewares/superadminMiddleware';
 import { UserService } from '../services/user-service';
+
+import { newUserSchema, updateUserSchema, getUserByDocumentSchema } from '../validators/user/index';
+
+import { makeResponse } from '../helpers/responseHelper';
+
 
 const userApi = express.Router();
 const service = new UserService();
 
 userApi.get('/', (req, res) => service.getUsers(req, res))
 
-userApi.get('/:id', (req, res) => service.getUser(req, res))
+userApi.get('/:document', async (req, res) => {   
+    try {
+        const {error, value} = await getUserByDocumentSchema.validateAsync(req.params)
 
-userApi.post("/", superadminMiddleware, (req, res) => service.createUser(req, res))
+        if (!error) {
+            const { document } = req.params
+            const user = await service.getUserByDocument(document)
+            if (user) {
+                return makeResponse(res, 200, user, true, null)
+            } else {
+                return makeResponse(res, 404, null, false, ['User not found'])
+            }
+        } else {
+            return makeResponse(res, 400, null, false, ['Bad request'])
+        }
+    } catch (err) {
+        return makeResponse(res, 500, err, false, ['Internal server error'])
+    }
+})
 
-userApi.put("/:id", superadminMiddleware, (req, res) => service.updateUser(req, res))
+userApi.post("/", async (req, res) => {   
+    try {
+        const {error, value} = await newUserSchema.validateAsync(req.body)
+    
+        if (!error) {
+            const { firstname, lastname, email, phone, document } = req.body
+            const userExists = await service.getUserByDocument(document)
+            if (!userExists) {
+                const success = await service.createUser(firstname, lastname, email, phone, document)
+                if (success) {
+                    const user = await service.getUserByDocument(document)
+                    return makeResponse(res, 201, user, true, null)
+                } else {
+                    return makeResponse(res, 400, null, false, ['Bad request'])
+                }
+            } else {
+                return makeResponse(res, 409, null, false, ['User already exists'])
+            }
+        }
+    } catch (err) {
+        return makeResponse(res, 500, null, false, ['Internal server error'])
+    }
+})
 
-userApi.delete("/:id", superadminMiddleware, (req, res) => service.deleteUser(req, res))
+userApi.put("/:id", async (req, res) => {   
+    try {
+        const {error, value} = await updateUserSchema.validateAsync(req.body)
+    
+        if (!error) {
+            const updatedUser = await service.updateUser(req, res)
+            return makeResponse(res, 200, updatedUser, true, null)
+        } else {
+            return makeResponse(res, 400, null, false, ['Bad request'])
+        }
+    } catch (err) {
+        return makeResponse(res, 500, null, false, ['Internal server error'])
+    }
+})
+
+userApi.delete("/:id", superadminMiddleware, async (req, res) => {   
+    try {
+        const {error, value} = await updateUserSchema.validateAsync(req.body)
+    
+        if (!error) {
+            const deletedUser = await service.deleteUser(req, res)
+            return makeResponse(res, 201, null, true, null)
+        } else {
+            return makeResponse(res, 400, null, false, ['Bad request'])
+        }
+    } catch (err) {
+        return makeResponse(res, 500, null, false, ['Internal server error'])
+    }
+})
 
 export = userApi
--- services/user-service.ts
+++ services/user-service.ts
@@ -16,8 +16,30 @@ export class UserService {
         return 1
     }
 
-    public createUser(req, res) {
-        return 1
+    public async createUser(
+        firstname: string,
+        lastname: string,
+        email: string,
+        phone: string,
+        document: string) {
+        return new Promise ((resolve, reject) => {
+            let sqlText = `
+                INSERT INTO users(firstname, lastname, email, phone, document)
+                VALUES ('${firstname}', '${lastname}', '${email}', '${phone}', '${document}');
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .insert(sqlText)
+                .then((success) => {
+                    if (success) {
+                        resolve(true);
+                    }
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
     }
 
     public updateUser(req, res) {
@@ -28,13 +50,36 @@ export class UserService {
         return 1
     }
 
-    public async getUserRole(user_id: number): Promise<string> {
+    public async getUserRole(user_id: number) {
         return new Promise ((resolve, reject) => {
-                const sqlText = `
+            const sqlText = `
                 SELECT user_type_name
                 FROM users, user_type
                 WHERE users.user_id = ${user_id} AND
-                users.user_type_id = user_type.user_type_id
+                users.user_type_id = user_type.user_type_id AND
+                users.deleted_at = NULL;
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .fetch(sqlText)
+                .then((data) => {
+                    let res = JSON.parse(JSON.stringify(data))
+                    res && res.length ? resolve(res[0]) : resolve(null);
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
+    }
+
+    public async getUserByDocument(document: string) {
+        return new Promise ((resolve, reject) => {
+            const sqlText = `
+                SELECT user_id, firstname, lastname, email, phone
+                FROM users
+                WHERE users.document = '${document}' AND
+                users.deleted_at IS NULL;
             `;
 
             MySqlConnection
