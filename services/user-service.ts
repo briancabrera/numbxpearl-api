0f24386c306419a1b237f7533864c2b0745e4598
Brian <briancabrer4@gmail.com>
2023-02-24 15:03:08 -0300

    user address crud on user service

--- services/user-service.ts
+++ services/user-service.ts
@@ -109,4 +109,109 @@ export class UserService {
                 })
         })
     }
+
+    public async getUserAddresses(
+        user_id: number
+    ) {
+        return new Promise((resolve, reject) => {
+            let sqlText = `
+                SELECT address.address_id, address.address, address.country_id, address.department_id,
+                country.country_name, department.department_name
+                FROM address, country, department
+                WHERE address.user_id = ${user_id} AND
+                country.country_id = address.country_id AND
+                department.department_id = address.address_id AND
+                address.deleted_at IS NULL;
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .fetch(sqlText)
+                .then((data) => {
+                    let res = JSON.parse(JSON.stringify(data))
+                    res && res.length ? resolve(res) : resolve(null);
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
+    }
+
+    public async createUserAddress(
+        country_id: number,
+        department_id: number,
+        address: string,
+        user_id: number) {
+        return new Promise ((resolve, reject) => {
+            let sqlText = `
+                INSERT INTO address(country_id, department_id, address, user_id)
+                VALUES (${country_id}, ${department_id}, '${address}', ${user_id});
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .runQuery(sqlText)
+                .then((success) => {
+                    if (success) {
+                        resolve(true);
+                    }
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
+    }
+
+    public async updateUserAddress(
+        address_id: number,
+        country_id: number,
+        department_id: number,
+        address: string) {
+        return new Promise ((resolve, reject) => {
+            let sqlText = `
+                UPDATE address
+                SET address.department_id = ${department_id},
+                address.address = '${address}',
+                address.updated_at = NOW()
+                WHERE address.address_id = ${address_id} AND
+                address.deleted_at IS NULL;
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .runQuery(sqlText)
+                .then((success) => {
+                    if (success) {
+                        resolve(true);
+                    }
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
+    }
+
+    public async deleteUserAddress(
+        address_id: number) {
+        return new Promise ((resolve, reject) => {
+            let sqlText = `
+                UPDATE address
+                SET address.deleted_at = NOW()
+                WHERE address.address_id = ${address_id} AND
+                address.deleted_at IS NULL;
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .runQuery(sqlText)
+                .then((success) => {
+                    if (success) {
+                        resolve(true);
+                    }
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
+    }
 }
