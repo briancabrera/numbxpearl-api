f2954a348fb6f40d28f7ab3093237a443b2660be
Brian <briancabrer4@gmail.com>
2023-02-24 03:58:01 -0300

    update category validator

--- validators/category/index.ts
+++ validators/category/index.ts
@@ -1,7 +1,9 @@
 import { getCategorySchema } from "./get-category-by-id";
 import { newCategorySchema } from "./new-category";
+import { updateCategorySchema } from "./update-category";
 
 export {
     getCategorySchema,
-    newCategorySchema
+    newCategorySchema,
+    updateCategorySchema
 }
