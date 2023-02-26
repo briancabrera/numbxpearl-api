7aff8922a18a25d1962d7c6c215f972392d31e96
Brian <briancabrer4@gmail.com>
2023-02-26 18:44:28 -0300

    crud collection service

--- services/collection-service.ts
+++ services/collection-service.ts
@@ -8,23 +8,159 @@ export class CollectionService {
         // private user: User
     ) {}
 
-    public getCollections(req, res) {
-        return 1
+    public async getCompanyCollections(
+        company_id: number
+    ) {
+        return new Promise((resolve, reject) => {
+            let sqlText = `
+                SELECT collection.collection_id, collection.collection_name, collection.available
+                FROM collection
+                WHERE collection.company_id = ${company_id} AND
+                collection.deleted_at IS NULL;
+            `;
+
+            // TODO!!!
+            // AGREGAR PRODUCTOS A DE LOS DROPS
+
+            MySqlConnection
+                .getInstance()
+                .fetch(sqlText)
+                .then((data) => {
+                    let res = JSON.parse(JSON.stringify(data))
+                    res && res.length ? resolve(res) : resolve(null);
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
     }
 
-    public getCollection(req, res) {
-        return 1
+    public async getCollection(
+        collection_id: number
+    ) {
+        return new Promise((resolve, reject) => {
+            let sqlText = `
+                SELECT collection.collection_id, collection.collection_name, collection.available
+                FROM collection
+                WHERE collection.company_id = ${collection_id} AND
+                collection.deleted_at IS NULL;
+            `;
+
+            // TODO!!!
+            // AGREGAR PRODUCTOS A DE LOS DROPS
+
+            MySqlConnection
+                .getInstance()
+                .fetch(sqlText)
+                .then((data) => {
+                    let res = JSON.parse(JSON.stringify(data))
+                    res && res.length ? resolve(res[0]) : resolve(null);
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
     }
 
-    public createCollection(req, res) {
-        return 1
+    private async getCollectionProducts(
+        collection_id: number
+    ) {
+        return new Promise((resolve, reject) => {
+            let sqlText = `
+                SELECT product.product_id, product.name, product.price, product.company_id, product.collection_id, category_id 
+                FROM product, collection
+                WHERE product.collection_id = ${collection_id} AND
+                product.deleted_at IS NULL;
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .fetch(sqlText)
+                .then((data) => {
+                    let res = JSON.parse(JSON.stringify(data))
+                    res && res.length ? resolve(res) : resolve(null);
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+        })
     }
 
-    public updateCollection(req, res) {
-        return 1
+    public async createCollection(
+        collection_name: string,
+        available: number,
+        company_id: number) {
+        return new Promise ((resolve, reject) => {
+            let sqlText = `
+                INSERT INTO collection(collection_name, available, company_id)
+                VALUES ("${collection_name}", ${available}, ${company_id});
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .runQuery(sqlText)
+                .then((success) => {
+                    if (success) {
+                        resolve(true);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    reject(error);
+                })
+        })
     }
 
-    public deleteCollection(req, res) {
-        return 1
+    public async updateCollection(
+        collection_id: number,
+        collection_name: string,
+        available: number) {
+        return new Promise ((resolve, reject) => {
+            let sqlText = `
+                UPDATE collection
+                SET collection.collection_name = "${collection_name}"
+                collection.available = ${available}
+                collection.updated_at = NOW()
+                WHERE collection.collection_id = ${collection_id} AND
+                collection.deleted_at IS NULL
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .runQuery(sqlText)
+                .then((success) => {
+                    if (success) {
+                        resolve(true);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    reject(error);
+                })
+        })
     }
-}
+
+    public async deleteCollection(
+        collection_id: number) {
+        return new Promise ((resolve, reject) => {
+            let sqlText = `
+                UPDATE collection
+                SET collection.deleted_at = NOW()
+                WHERE collection.collection_id = ${collection_id} AND
+                collection.deleted_at IS NULL
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .runQuery(sqlText)
+                .then((success) => {
+                    if (success) {
+                        resolve(true);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    reject(error);
+                })
+        })
+    }
