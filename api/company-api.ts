cf4db80828eda0a8e6437dcf9387cc67b1238089
Brian <briancabrer4@gmail.com>
2023-02-27 20:14:50 -0300

    coupon api

--- api/company-api.ts
+++ api/company-api.ts
@@ -3,8 +3,10 @@ import { makeResponse } from '../helpers/responseHelper';
 import { superadminMiddleware } from '../middlewares/superadminMiddleware';
 import { CollectionService } from '../services/collection-service';
 import { CompanyService } from '../services/company-service';
+import { CouponService } from '../services/coupon-service';
 import { ProductService } from '../services/product-service';
 import { getCompanyCollectionsSchema } from '../validators/collection';
+import { getCompanyDiscountCouponsSchema } from '../validators/discount_coupon/get-company-coupons';
 import { getCompanyProductsSchema } from '../validators/product';
 
 const companyApi = express.Router();
@@ -19,7 +21,7 @@ companyApi.put("/:id", superadminMiddleware, (req, res) => res.status(503).send(
 
 companyApi.delete("/:id", superadminMiddleware, (req, res) => res.status(503).send())
 
-companyApi.get('/:company_id/products', async (req, res) => {
+companyApi.get('/:company_id/products', superadminMiddleware, async (req, res) => {
     try {
         const {error, value} = await getCompanyProductsSchema.validateAsync(req.params)
         const service = new ProductService();
@@ -52,6 +54,27 @@ companyApi.get('/:company_id/collections', async (req, res) => {
                 return makeResponse(res, 200, collections, true, null)
             } else {
                 return makeResponse(res, 404, null, false, ['No collections found for the company'])
+            }
+        } else {
+            return makeResponse(res, 400, null, false, ['Bad request'])
+        }
+    } catch(err) {
+        return makeResponse(res, 500, null, false, ['Internal server error'])
+    }
+})
+
+companyApi.get('/:company_id/coupons', superadminMiddleware, async (req, res) => {
+    try {
+        const {error, value} = await getCompanyDiscountCouponsSchema.validateAsync(req.params)
+        const service = new CouponService();
+        
+        if (!error) {
+            const { company_id } = req.params
+            const coupons = await service.getCompanyDiscountCoupons(company_id)
+            if (coupons) {
+                return makeResponse(res, 200, coupons, true, null)
+            } else {
+                return makeResponse(res, 404, null, false, ['No coupons found for the company'])
             }
         } else {
             return makeResponse(res, 400, null, false, ['Bad request'])
--- api/discount-coupon-api.ts
+++ api/discount-coupon-api.ts
@@ -4,18 +4,110 @@ import { CouponService } from '../services/coupon-service';
 
 import { adminMiddleware } from '../middlewares/adminMiddleware';
 import { superadminMiddleware } from '../middlewares/superadminMiddleware';
+import { getDiscountCouponByCodeSchema } from '../validators/discount_coupon/get-coupon';
+import { makeResponse } from '../helpers/responseHelper';
+import { newCouponSchema } from '../validators/new-coupon';
+import { updateDiscountCouponSchema } from '../validators/discount_coupon/update-coupon';
+import { deleteDiscountCouponSchema } from '../validators/discount_coupon/delete-coupon';
 
 const couponApi = express.Router();
 const service = new CouponService();
 
-couponApi.get('/', superadminMiddleware, (req, res) => service.getCoupons(req, res))
+couponApi.get('/:coupon_code', superadminMiddleware, async (req, res) => {   
+    try {
+        const {error, value} = await getDiscountCouponByCodeSchema.validateAsync(req.params)
 
-couponApi.get('/:id', (req, res) => service.getCoupon(req, res))
+        if (!error) {
+            const { coupon_code } = req.params
+            const coupon = await service.getDiscountCouponByCode(coupon_code)
+            if (coupon) {
+                return makeResponse(res, 200, coupon, true, null)
+            } else {
+                return makeResponse(res, 404, null, false, ['Coupon not found'])
+            }
+        } else {
+            return makeResponse(res, 400, null, false, ['Bad request'])
+        }
+    } catch (err) {
+        return makeResponse(res, 500, err, false, ['Internal server error'])
+    }
+})
 
-couponApi.post("/", superadminMiddleware, (req, res) => service.createCoupon(req, res))
+couponApi.post("/", superadminMiddleware, async (req, res) => {   
+    try {
+        const {error, value} = await newCouponSchema.validateAsync(req.body)
+    
+        if (!error) {
+            let { 
+                company_id,
+                coupon_code,
+                percentage,
+                valid_until,
+                is_active } = req.body
+            const success = await service.createDiscountCoupon(company_id, coupon_code.trim(), percentage, valid_until.trim(), is_active)
+            if (success) {
+                return makeResponse(res, 201, null, true, null)
+            } else {
+                return makeResponse(res, 400, null, false, ['Bad request'])
+            }
+        }
+    } catch (err) {
+        return makeResponse(res, 500, null, false, ['Internal server error'])
+    }
+})
 
-couponApi.put("/:id", superadminMiddleware, (req, res) => service.updateCoupon(req, res))
+couponApi.put("/:coupon_id", superadminMiddleware, async (req, res) => {   
+    try {
+        const {error, value} = await updateDiscountCouponSchema.validateAsync({...req.body, ...req.params})
+        if (!error) {
+            let { 
+                coupon_code,
+                percentage,
+                valid_until,
+                is_active } = req.body
+            let { coupon_id } = req.params
+            const coupon = await service.getDiscountCouponById(coupon_id);
+            if (coupon) {
+                const success = await service.updateDiscountProduct(coupon_id, coupon_code.trim(), percentage, valid_until.trim(), is_active)
+                if (success) {
+                    return makeResponse(res, 200, null, true, null)
+                } else {
+                    return makeResponse(res, 409, null, false, ['Conflict'])
+                }
+            } else {
+                return makeResponse(res, 404, null, false, ['Coupon not found'])
+            }
+        } else {
+            return makeResponse(res, 400, null, false, ['Bad request'])
+        }
+    } catch (err) {
+        return makeResponse(res, 500, null, false, ['Internal server error'])
+    }
+})
 
-couponApi.delete("/:id", superadminMiddleware, (req, res) => service.deleteCoupon(req, res))
+couponApi.delete("/:coupon_id", superadminMiddleware, async (req, res) => {   
+    try {
+        const {error, value} = await deleteDiscountCouponSchema.validateAsync(req.params)
+    
+        if (!error) {
+            let { coupon_id } = req.params
+            const coupon = await service.getDiscountCouponById(coupon_id)
+            if (coupon) {
+                const success = await service.deleteDiscountCoupon(coupon_id)
+                if (success) {
+                    return makeResponse(res, 200, null, true, null)
+                } else {
+                    return makeResponse(res, 409, null, false, ['Conflict'])
+                }
+            } else {
+                return makeResponse(res, 404, null, false, ['Coupon not found'])
+            }
+        } else {
+            return makeResponse(res, 400, null, false, ['Bad request'])
+        }
+    } catch (err) {
+        return makeResponse(res, 500, null, false, ['Internal server error'])
+    }
+})
 
 export = couponApi
