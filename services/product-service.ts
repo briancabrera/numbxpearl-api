956939defc95cfae37aca0f23efa18f8dae999bf
Brian <briancabrer4@gmail.com>
2023-03-01 23:08:48 -0300

    product service update getproduct added variants to product

--- services/product-service.ts
+++ services/product-service.ts
@@ -66,11 +66,45 @@ export class ProductService {
     public async getProduct(
         product_id: number
     ) {
-        return new Promise((resolve, reject) => {
+        try {
             let sqlText = `
                 SELECT product.product_id, product.name, product.price, product.company_id, product.collection_id, category_id 
                 FROM product
                 WHERE product.product_id = ${product_id} AND
+                product.deleted_at IS NULL;
+            `;
+
+            let product = await MySqlConnection   
+                .getInstance()
+                .fetch(sqlText)
+                .then((data) => JSON.parse(JSON.stringify(data))[0]);
+
+            if (product) {
+                product.variants = await this.getProductVariants(product_id)
+            }
+
+            return product
+        } catch (err) {
+            console.log(err)
+            return null
+        }
+    }
+
+    public async getVariantWithProduct(
+        product_id: number,
+        color: string,
+        size: string
+    ) {
+        return new Promise((resolve, reject) => {
+            let sqlText = `
+                SELECT product.product_id, product.name, product.price, product.company_id, product.collection_id, category_id,
+                product_variant.variant_id, product_variant.color, product_variant.size
+                FROM product, product_variant
+                WHERE product.product_id = ${product_id} AND
+                product_variant.product_id = ${product_id} AND
+                product_variant.size = '${size}' AND
+                product_variant.color = '${color}' AND
+                product_variant.deleted_at IS NULL AND
                 product.deleted_at IS NULL;
             `;
 
@@ -79,12 +113,34 @@ export class ProductService {
                 .fetch(sqlText)
                 .then((data) => {
                     let res = JSON.parse(JSON.stringify(data))
-                    res && res.length ? resolve(res) : resolve(null);
+                    res && res.length ? resolve(res[0]) : resolve(null);
                 })
                 .catch((error) => {
                     reject(error);
                 })
         })
+    }
+
+    private async getProductVariants(
+        product_id: number,
+    ) {
+        try {
+            let sqlText = `
+                    SELECT product_variant.variant_id, product_variant.color, product_variant.size
+                    FROM product_variant
+                    WHERE product_variant.product_id = ${product_id} AND
+                    product_variant.deleted_at IS NULL;
+                `
+            let variants = await MySqlConnection   
+                .getInstance()
+                .fetch(sqlText)
+                .then((data) => JSON.parse(JSON.stringify(data)));
+
+            return variants
+        } catch (err) {
+            console.log(err)
+            return null
+        }
     }
 
     public async createProduct(
@@ -113,6 +169,49 @@ export class ProductService {
                     console.log(error);
                     reject(error);
                 })
+        })
+    }
+
+    public async createProductVariants(
+        product_id: number,
+        colors: string[],
+        sizes: string[]) {
+        try {
+            for (let i = 0; i < colors.length; i++) {
+                for (let j = 0; j < sizes.length; j++) {
+                    await this.createProductVariant(product_id, colors[i], sizes[j])
+                }
+            }
+            return true
+        } catch(err) {
+            console.log(err)
+            return null
+        }
+    }
+
+    public async createProductVariant(
+        product_id: number,
+        color: string,
+        size: string) {
+        return new Promise ((resolve, reject) => {
+            let sqlText = `
+                INSERT INTO product_variant(product_id, color, size)
+                VALUES (${product_id}, "${color}", "${size}");
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .runQuery(sqlText)
+                .then((success) => {
+                    if (success) {
+                        resolve(true);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    reject(error);
+                })
+
         })
     }
 
@@ -163,6 +262,35 @@ export class ProductService {
                 SET product.deleted_at = NOW()
                 WHERE product.product_id = ${product_id} AND
                 product.deleted_at IS NULL;
+            `;
+
+            MySqlConnection
+                .getInstance()
+                .runQuery(sqlText)
+                .then((success) => {
+                    if (success) {
+                        resolve(true);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    reject(error);
+                })
+        })
+    }
+
+    public async deleteProductVariant(
+        product_id: number,
+        color: string,
+        size: string) {
+        return new Promise ((resolve, reject) => {
+            let sqlText = `
+                UPDATE product_variant
+                SET product_variant.deleted_at = NOW()
+                WHERE product_variant.product_id = ${product_id} AND
+                product_variant.color = "${color}" AND
+                product_variant.size = "${size}" AND
+                product_variant.deleted_at IS NULL;
             `;
 
             MySqlConnection
