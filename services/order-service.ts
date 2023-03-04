0e099fc61e2a8c9d40ff530b7216bee2aec3b839
Brian <briancabrer4@gmail.com>
2023-03-03 22:04:31 -0300

    order created in db

--- services/order-service.ts
+++ services/order-service.ts
@@ -19,8 +19,29 @@ export class OrderService {
         return 1
     }
 
-    public getOrder(req, res) {
-        return 1
+    private async getOrderByMpReference(
+        mp_reference: string
+    ) {
+        return new Promise((resolve, reject) => {
+            let sqlText = `
+                SELECT purchase_order.order_id
+                FROM purchase_order
+                WHERE purchase_order.mp_reference = "${mp_reference}" AND
+                purchase_order.deleted_at IS NULL;
+            `
+
+            MySqlConnection.getInstance()
+                .fetch(sqlText)
+                .then(data => {
+                    let res = JSON.parse(JSON.stringify(data))
+                    res && res.length ? resolve(res[0]) : reject(null)
+                })
+                .catch(err => {
+                    console.log(err)
+                    reject(err)
+                })
+        
+        })
     }
 
     public async createOrder(
@@ -50,16 +71,43 @@ export class OrderService {
                 payer.address.address_id = address['address_id']
             }
 
+            let amount = 0;
             let preferenceProducts = []
             for (let i = 0; i < products.length; i++) {
                 let product = await this.productService.getVariantWithProduct(products[i].product_id, products[i].color, products[i].size)
+                amount += product['price']
                 preferenceProducts.push(product)
             }
 
             const preference = await this.mpService.createPreference(preferenceProducts, payer, company_id, coupon_id)
 
             if (preference) {
-                return preference    
+                let sqlText = `
+                    INSERT INTO purchase_order(status, mp_reference, amount, user_id, company_id, coupon_id)
+                    VALUES ("PENDING", "${preference.body.external_reference}", ${amount}, ${payer.user_id}, ${company_id}, ${coupon_id})
+                `
+                let orderCreated = await MySqlConnection
+                    .getInstance()
+                    .runQuery(sqlText)
+
+                if (orderCreated) {
+                    let order = await this.getOrderByMpReference(preference.body.external_reference)
+                    for (let i = 0; i < preferenceProducts.length; i++) {
+                        let sqlText = `
+                            INSERT INTO order_products(order_id, variant_id)
+                            VALUES (${order['order_id']}, ${preferenceProducts[i].variant_id})
+                        `
+                        let productRegistered = await MySqlConnection
+                            .getInstance()
+                            .runQuery(sqlText)
+                        
+                        if (!productRegistered) {
+                            return false
+                        }
+                    }
+
+                    return preference.body.sandbox_init_point
+                }
             }
         } catch (err) {
             console.log(err)
