54625d20c73f30e89cca5622590c0bee536466ad
Brian <briancabrer4@gmail.com>
2023-03-06 03:00:58 -0300

    update and delete user endpoints

--- api/user-api.ts
+++ api/user-api.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import { superadminMiddleware } from '../middlewares/superadminMiddleware';
 import { UserService } from '../services/user-service';
 
-import { newUserSchema, updateUserSchema, getUserByDocumentSchema } from '../validators/user/index';
+import { newUserSchema, updateUserSchema, getUserByDocumentSchema, deleteUserSchema } from '../validators/user/index';
 import { getAddressesSchema, updateAddressSchema, createAddressSchema, deleteAddressSchema } from '../validators/address';
 
 import { makeResponse } from '../helpers/responseHelper';
@@ -50,7 +50,12 @@ userApi.post("/", async (req, res) => {
         const {error, value} = await newUserSchema.validateAsync(req.body)
     
         if (!error) {
-            let { firstname, lastname, email, phone, document } = req.body
+            let { 
+                firstname,
+                lastname, 
+                email, 
+                phone, 
+                document } = req.body
             const userExists = await service.getUserByDocument(document)
             if (!userExists) {
                 const success = await service.createUser(firstname.trim(), lastname.trim(), email.trim(), phone.trim(), document.trim())
@@ -69,41 +74,59 @@ userApi.post("/", async (req, res) => {
     }
 })
 
+userApi.put("/:user_id", superadminMiddleware, async (req, res) => {   
+    try {
+        const {error, value} = await updateUserSchema.validateAsync({...req.body, ...req.params})
+        if (!error) {
+            let { 
+                firstname,
+                lastname,
+                email,
+                phone } = req.body
+            let { user_id } = req.params
+            const user = await service.getUserById(user_id);
+            if (user) {
+                const success = await service.updateUser(user_id, firstname.trim(), lastname.trim(), email.trim(), phone.trim())
+                if (success) {
+                    return makeResponse(res, 200, null, true, null)
+                } else {
+                    return makeResponse(res, 409, null, false, ['Conflict'])
+                }
+            } else {
+                return makeResponse(res, 404, null, false, ['User not found'])
+            }
+        } else {
+            return makeResponse(res, 400, null, false, ['Bad request'])
+        }
+    } catch (err) {
+        return makeResponse(res, 500, null, false, ['Internal server error'])
+    }
+})
 
-// TODO
-// userApi.put("/:id", async (req, res) => {   
-//     try {
-//         const {error, value} = await updateUserSchema.validateAsync(req.body)
+userApi.delete("/:user_id", superadminMiddleware, async (req, res) => {   
+    try {
+        const {error, value} = await deleteUserSchema.validateAsync(req.params)
     
-//         if (!error) {
-//             const updatedUser = await service.updateUser(req, res)
-//             return makeResponse(res, 200, updatedUser, true, null)
-//         } else {
-//             return makeResponse(res, 400, null, false, ['Bad request'])
-//         }
-//     } catch (err) {
-//         return makeResponse(res, 500, null, false, ['Internal server error'])
-//     }
-// })
-
-// userApi.delete("/:id", superadminMiddleware, async (req, res) => {   
-//     try {
-//         const {error, value} = await updateUserSchema.validateAsync(req.body)
-    
-//         if (!error) {
-//             const deletedUser = await service.deleteUser(req, res)
-//             if (deletedUser) {
-//                 return makeResponse(res, 200, null, true, null)
-//             } else {
-//                 return makeResponse(res, 409, null, false, ['Conflict'])
-//             }
-//         } else {
-//             return makeResponse(res, 400, null, false, ['Bad request'])
-//         }
-//     } catch (err) {
-//         return makeResponse(res, 500, null, false, ['Internal server error'])
-//     }
-// })
+        if (!error) {
+            let { user_id } = req.params
+            const user = await service.getUserById(user_id)
+            if (user) {
+                const success = await service.deleteUser(user_id)
+                if (success) {
+                    return makeResponse(res, 200, null, true, null)
+                } else {
+                    return makeResponse(res, 409, null, false, ['Conflict'])
+                }
+            } else {
+                return makeResponse(res, 404, null, false, ['User not found'])
+            }
+        } else {
+            return makeResponse(res, 400, null, false, ['Bad request'])
+        }
+    } catch (err) {
+        return makeResponse(res, 500, null, false, ['Internal server error'])
+    }
+})
 
 userApi.get("/:user_id/address", async (req, res) => {
     try {
