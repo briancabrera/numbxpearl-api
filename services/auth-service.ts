1a0e276b9f59f6a7c4573acb2236d056e52ff08d
Brian Cabrera <briancabrer4@gmail.com>
2023-03-28 19:41:20 -0300

    login method created authService

--- services/auth-service.ts
+++ services/auth-service.ts
@@ -11,4 +11,35 @@ export class AuthService {
 
     constructor () {}
 
+    public async login(email: string, password: string) {
+        const data = {
+            success: true,
+            status: 200,
+            token: ''
+        }
+
+        const user = await this.userService.getUserByEmail(email);
+
+        if (user) {
+            const role = await this.userService.getUserRole(user['user_id'])
+
+            if (role && role['user_type_name'] === Roles.SUPERADMIN ) {
+                const match = compare(password, user['password'])
+
+                if (match) {
+                    data.token = jsonwebtoken.sign({
+                        email: user['email'],
+                        user_id: user['user_id'],
+                        document: user['document']
+                    }, jwtSecret, { expiresIn: '1h' })
+                }
+            }
+        } else {
+            data.success = false;
+            data.status = 400;
+        }
+
+        return data
+    }
+
 }
